519c26b477a97ad204c8014bbbb7cc5e7a1dcf36
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-04-01 12:05:06 +0500

    Add data type examples and comments in Uzbek
    Updated `Program.cs` to include comments and examples for various C# data types, including value types, floating-point types, and string handling. Added sample code for user input and arithmetic operations with int, float, double, and decimal types, as well as string concatenation. Comments are provided in Uzbek for better context.

--- BasicLessons/Program.cs
+++ BasicLessons/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿//Value type
+//int, float, double, decimal, char, bool, enum, struct, nullable types
+
+
+//natural sonlar bilan ishlovchi type
+//int son1;
+//int son2 = 10;
+
+//son1 = int.Parse(Console.ReadLine());
+//Console.WriteLine(son1 + son2);
+
+
+//kasrli va natural sonlar bilan ishlovchi type
+//float son1 = float.Parse(Console.ReadLine());
+//float son2 = 3.14f;
+
+//Console.WriteLine(son1 + son2);
+
+
+//kasrli va natural sonlar bilan ishlovchi type
+//double son1 = double.Parse(Console.ReadLine());
+//double son2 = 5.4;
+
+//Console.WriteLine($"natija: {son1 + son2}, bizning xizmatimizdan foydalanganingiz uchun sps");
+
+
+//o'ta katta kasrli va natural sonlar bilan ishlovchi type
+//decimal son1 = decimal.Parse(Console.ReadLine());
+//decimal son2 = 1.0123781632836172368e19m;
+
+//Console.WriteLine($"natija: {son1 + son2}");
+
+
+//matn bilan ishlovchi type
+//string matn1 = Console.ReadLine();
+//string matn2 = "Assalomu aleykum!";
+
+//Console.WriteLine($"{matn2} {matn1}, xush kelibsiz bro.");
