ec6ed0dad345d3f528239b1290faeccaa44cca92
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-04-23 10:58:15 +0500

    Add Methods project with basic arithmetic operations
    Updated the solution file to include a new project called "Methods" and its corresponding project file `Methods\Methods.csproj`. Configurations for Debug and Release builds were added for the new project.
    
    Created `Methods.csproj` targeting .NET 9.0 with modern C# features. Added `Program.cs` containing a `Mathematics` class for basic arithmetic operations and a console interface for user interaction.

---
+++ Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Mathematics
+{
+    public int add(int a, int b) => a + b;
+    public int subtract(int a, int b) => a - b;
+    public int multiply(int a, int b) => a * b;
+    public int divide(int a, int b) => a / b;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        //Mathematics sinfidan yangi object yaratildi
+        Mathematics math = new Mathematics();
+
+        int a, b;
+        char belgi;
+
+        while (true)
+        {
+            Console.Write("+\t-\t*\t/\t-lardan bittasini tanlang: ");
+            belgi = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("1-sonni kiriting: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2-sonni kiriting: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int res = belgi switch
+            {
+                '+' => math.add(a, b),
+                '-' => math.subtract(a, b),
+                '*' => math.multiply(a, b),
+                '/' => math.divide(a, b),
+                _ => throw new InvalidOperationException("Noto'g'ri amal tanlandi")
+            };
+
+            Console.WriteLine($"Natija: {res}");
+        }
+    }
+}
