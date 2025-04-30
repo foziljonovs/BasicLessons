44ef60e2e8297737dec0895cee79c37c70b0c450
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-04-30 11:14:12 +0500

    Add Constructor project with Product and Category classes
    - Introduced a new project "Constructor" in the solution.
    - Added `Product` class with multiple constructors and properties.
    - Added `Category` class with properties for name and description.
    - Updated `Program.cs` to demonstrate usage of new classes.

---
+++ Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using Constructor;
+
+Product product = new Product("Shahar", "Juda shirin lekin juda ziyonli", 20000);
+Console.WriteLine($"mahsulot nomi: {product.GetName()}," +
+    $"\thaqida: {product.GetDescription()}" +
+    $"\tnarxi: {product.GetPrice()}");
+
+Product product1 = new Product("oqqand", 22000);
+Console.WriteLine($"mahsulot nomi: {product1.GetName()}," +
+    $"\thaqida: {product1.GetDescription()}" +
+    $"\tnarxi: {product1.GetPrice()}");
+
+
+
+Category category = new Category();
+category.Name = "Meva";
+category.Description = "Mevalar juda foydali";
+
+Console.WriteLine($"1 - category, name : {category.Name}, description : {category.Description}");
+
+
+Category category2 = new Category()
+{
+    Name = "Ichimlik suvi",
+    Description = "Tabiiy ichimlik suvi"
+};
+
+Console.WriteLine($"2 - category, name : {category2.Name}, description : {category2.Description}");
