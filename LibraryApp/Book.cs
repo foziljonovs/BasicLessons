38df973c72d20eb0bac3bff208b8a20491ae0198
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-06-04 11:04:43 +0500

    Add LibraryApp project with book management features
    - Updated `BasicLessons.sln` to include `LibraryApp` project.
    - Created `Book` class for managing book properties.
    - Introduced `LibraryService` class for book operations.
    - Added `LibraryApp.csproj` with .NET application settings.
    - Modified `Program.cs` to implement a console interface for library interactions.

---
+++ LibraryApp/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace LibraryApp;
+
+public class Book
+{
+    private string Title { get; set; }
+    private string Author { get; set; }
+    private int Year { get; set; }
+    private bool IsAvailable { get; set; } = true;
+
+    public Book(string title, string author, int year, bool isAvailable)
+    {
+        this.Title = title;
+        this.Author = author;
+        this.Year = year;
+        this.IsAvailable = isAvailable;
+    }
+
+    public void SetAvailability(bool isAvailable)
+        => this.IsAvailable = isAvailable;
+
+    public string GetTitle()
+        => this.Title;
+
+    public string GetAuthor()
+        => this.Author;
+
+    public int GetYear()
+        => this.Year;
+
+    public bool GetAvailability()
+        => this.IsAvailable;
+}
---
+++ LibraryApp/LibraryService.cs
@@ -0,0 +1,36 @@
+﻿namespace LibraryApp;
+
+public static class LibraryService
+{
+    private static List<Book> books = new List<Book>();
+
+    public static void AddBook(Book book)
+        => books.Add(book);
+
+    public static List<Book> GetBooks()
+        => books;
+
+    public static bool BorrowBook(string title)
+    {
+        var book = books.FirstOrDefault(b => b.GetTitle() == title &&
+            b.GetAvailability() == true);
+
+        if (book is null)
+            return false;
+
+        book.SetAvailability(false);
+        return true;
+    }
+
+    public static bool ReturnBook(string title)
+    {
+        var book = books.FirstOrDefault(b => b.GetTitle() == title &&
+            b.GetAvailability() == false);
+
+        if (book is null)
+            return false;
+
+        book.SetAvailability(true);
+        return true;
+    }
+}
