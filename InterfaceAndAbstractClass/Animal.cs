d7725834757ad38accb98145eda7a9ef1ffd21da
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-06-09 10:55:06 +0500

    Add ILibrary interface and enhance BookServiceTwo
    - Updated BookServiceTwo to implement ILibrary, adding GetAllBooks method.
    - Commented out old code in Program.cs and demonstrated new usage of ILibrary and BookServiceTwo.
    - Introduced Animal class as an abstract base class with Lion subclass implementing Speak and overriding Eat methods.
    - Defined ILibrary interface with GetAllBooks method signature.

---
+++ InterfaceAndAbstractClass/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+namespace InterfaceAndAbstractClass;
+
+public abstract class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public abstract void Speak();
+    public virtual void Eat()
+        => Console.WriteLine($"{Name} is eating.");
+}
+
+public class Lion : Animal
+{
+    public Lion(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void Speak()
+        => Console.WriteLine($"{Name} roars!");
+
+    public override void Eat()
+        => Console.WriteLine($"{this.Name} eating.");
+}
--- InterfaceAndAbstractClass/BookServiceTwo.cs
+++ InterfaceAndAbstractClass/BookServiceTwo.cs
@@ -1,7 +1,10 @@
 ﻿namespace InterfaceAndAbstractClass;
 
-public class BookServiceTwo : IBook
+public class BookServiceTwo : IBook, ILibrary
 {
+    public void GetAllBooks()
+        => Console.WriteLine("Getting all books in BookServiceTwo...");
+
     public void Read()
         => Console.WriteLine("Reading the book in BookServiceTwo...");
     public void Write(string text)
