127d5dea88b494e0f9a0fa456e559fb5db6bee95
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-06-14 13:04:31 +0500

    Add Polymorphism project with example implementation
    - Introduced a new project "Polymorphism" in `BasicLessons.sln`.
    - Updated solution configuration for Debug and Release builds.
    - Created `Polymorphism.csproj` with .NET SDK and necessary properties.
    - Added `Program.cs` demonstrating static and dynamic polymorphism.
    - Implemented base class `Shakl` and derived class `Uchburchak` in `Shakl.cs`.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,31 @@
+﻿//Polymorphism 2 qismga bolinadi
+//1.static polymorphism(overload) - compile time polymorphism
+//2.dynamic polymorphism(override) - run time polymorphism
+
+//class Program
+//{
+//    static int addTwoNumbers(int a, int b)
+//        => a + b;
+
+//    static double addTwoNumbers(double a, double b)
+//        => a + b;
+
+//    static void Main(string[] args)
+//    {
+//        int a, b;
+//        Console.Write("a - sonni kiriting: ");
+//        a = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("b - sonni kiriting: ");
+//        b = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine($"{a} + {b} = {addTwoNumbers(a, b)}");
+//        Console.WriteLine($"{5.1} + {3} = {addTwoNumbers(5.1, 3)}");
+//    }
+//}
+
+
+using Polymorphism;
+
+Shakl uchburchak = new Uchburchak();
+uchburchak.Chizish();
