a0692c557c51ae99e2f7185e9e81d5c60736a079
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-06-19 12:08:07 +0500

    Add Enumeration project with enums and Product class
    Updated the solution to include a new project "Enumeration" and its project file.
    Created `Cities.cs` to define enums for cities, days, and countries.
    Added `Enumeration.csproj` as a console application targeting .NET 9.0.
    Introduced a `Product` class in `Product.cs` with properties and a method to display product information.
    Modified `Program.cs` to include the `Enumeration` namespace and added commented-out examples for usage.

---
+++ Enumeration/Cities.cs
@@ -0,0 +1,29 @@
+﻿namespace Enumeration;
+
+public enum Cities
+{
+    Margilon = 1,
+    Qoqon,
+    Quva,
+    Oltiariq
+}
+
+public enum Day
+{
+    Dushanba = 1,
+    Seshanba,
+    Chorshanba,
+    Payshanba,
+    Juma,
+    Shanba,
+    Yakshanba
+}
+
+public enum Made
+{
+    Uzbekistan = 1,
+    China,
+    Russia,
+    Turkey,
+    Japan
+}
---
+++ Enumeration/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+
+namespace Enumeration;
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public double Price { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Made Made { get; set; }
+
+    public Product()
+    {
+        this.Name = "Qora kofe";
+        this.Brand = "Nescafe";
+        this.Price = 10000;
+        this.CreatedDate = DateTime.Now;
+        this.Made = (Made)1;
+    }
+
+    public void GetProduct()
+        => Console.WriteLine($"name: {Name}, brand: {Brand}, price: {Price}, made: {Made}");
+}
